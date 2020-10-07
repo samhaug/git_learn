762077d72c54a8a7c06b664374bb84ba0c3abda6
samhaug <haugland21@gmail.com>
2020-10-07 15:26:12 -0600

    added malloc

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int main(int argc, char **argv){
-   int i;
+   int *i;
+   
+   i = malloc(40*sizeof(int));
+   printf("%x\n",i);
+   printf("%o\n",i);
+   printf("%d\n",i);
+   
    printf("Hello World\n");
    printf("%s\n",*(argv+1));
    
