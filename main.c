3f5ca0956f6631ee8da893e97a9665af4259875c
samhaug <haugland21@gmail.com>
2020-10-08 11:59:00 -0600

    global pointer

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
+int *a;
+
+void f();
 int main(){
    int j=30;
     
@@ -11,4 +14,10 @@ int main(){
        printf("%d\n",i);
    }
    printf("This is the second new branch\n");
+   f();
+}
+
+void f(){
+    a = malloc(sizeof(int));
+    printf("global %d\n",a);
 }
