7b9831b8a03e07add0a454ba256aa25252227519
samhaug <haugland21@gmail.com>
2020-10-08 10:27:00 -0600

    added void

--- main.c
+++ main.c
@@ -4,7 +4,10 @@
 
 int main(int argc, char **argv){
    int *i;
+   void *v1,*v2;
    
+   printf("void 1: %x\n",v1);
+   printf("void 2: %x\n",v2);
    i = malloc(40*sizeof(int));
    printf("%x\n",i);
    printf("%o\n",i);
