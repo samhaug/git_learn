a92859122a7103c1c2a334fc2b52af26497aeaed
samhaug <haugland21@gmail.com>
2020-10-10 19:56:11 -0600

    int addresses

--- main.c
+++ main.c
@@ -3,12 +3,10 @@
 #include <math.h>
 
 int main(int argc, char **argv){
-   int *i;
-   void *v1,*v2;
+   int i;
+   FILE *ifl;
+   i=40;
    
-   printf("void 1: %x\n",v1);
-   printf("void 2: %x\n",v2);
-   i = malloc(40*sizeof(int));
    printf("%x\n",i);
    printf("%o\n",i);
    printf("%d\n",i);
