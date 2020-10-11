8da8c69cadc50b6a0fbab65cffd9df90e7ce2c05
samhaug <haugland21@gmail.com>
2020-10-11 09:52:24 -0600

    dynamic allocating

--- main.c
+++ main.c
@@ -6,21 +6,27 @@ int main(int argc, char **argv){
 
    int a[1000];
    FILE *ifl;
-   if (argc != 2){
-      printf("Need a file name\n");
+   if (argc != 3){
+      printf("Need a file name and number of lines\n");
       exit(0);
    }
 
-   ifl = fopen(*(argv+1),"r");
+   if (!(ifl = fopen(*(argv+1),"r"))){
+      printf("error opening file\n");
+   }
+
+   int num = atoi(*(argv+2));
+   int *b = malloc((num+1)*sizeof(int));
+  
 
    int i=0;
    while(!feof(ifl)){
-      fscanf(ifl,"%d",a+i);
+      fscanf(ifl,"%d",b+i);
       i++;
    }
 
    for (int j=0;j<i;j++){
-       printf("%d\n",a[j]);
+       printf("%d\n",b[j]);
    }
 
 }
