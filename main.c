ec23e94748b298be969e6b9ca006ea65aa1525e2
samhaug <haugland21@gmail.com>
2020-10-11 09:11:18 -0600

    book titles

--- main.c
+++ main.c
@@ -4,6 +4,16 @@
 
 int main(int argc, char **argv){
 
-   printf("This is the end of the script\n");
+   char *titles[] = {"A tale of two cities", "Moby Dick"};
+   printf("%s\n",*titles);
+   printf("%s\n",*(titles+1));
+
+   int vector[] = {234, 543, 32, 54};
+   int *p = vector;
+
+   for (int i=0;i<4;i++){
+      printf("vec: %x %d\n",p,*p);
+      p++;
+   }
 }
 
